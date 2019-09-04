101e4f68c46ed37d028dcb846dd8e686716aede0
fakhrad <fakhrad@openratio.com>
2019-09-04 12:15:38 +0430

    soace model updated

--- src/models/space.js
+++ src/models/space.js
@@ -1,31 +1,31 @@
-
 /**
  * Module dependencies.
  */
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 /**
  * Schema definitions.
  */
 
- const space = new Schema({
-  name : {type : String, required : true, max : 150, min : 3},
-  description : {type : String, max : 256},
-  image : {type : Object},
-  type : {type : String},
-  owner : {type: Schema.Types.ObjectId, ref: 'AdminUsers' , required : true},
-  roles : [],
-  locales : [],
-  webhooks : []
+const space = new Schema({
+  name: { type: String, required: true, max: 150, min: 3 },
+  description: { type: String, max: 256 },
+  image: { type: Object },
+  type: { type: String },
+  notification_email: { type: String },
+  owner: { type: Schema.Types.ObjectId, ref: "AdminUsers", required: true },
+  roles: [],
+  locales: [],
+  webhooks: []
 });
 
 space.methods.viewModel = function(cb) {
   return {
-      id : this._id,
-      roles : this.roles,
-      locales : this.locales,
-      name : this.name
-  }
+    id: this._id,
+    roles: this.roles,
+    locales: this.locales,
+    name: this.name
+  };
 };
-module.exports = mongoose.model('Space', space);
+module.exports = mongoose.model("Space", space);
